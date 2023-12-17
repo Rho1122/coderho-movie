d516b6c8aba0ceb57e8d80aa94a76a581dd9151f
Rho1122 <kingrolsmack1@gmail.com>
2023-12-17 21:46:56 +0200

    NavBar Completed

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,47 @@
-const NavBar = () => {
-  return <div></div>;
+import { Link } from "react-router-dom";
+
+interface NavBarProps {
+  navList: string[];
+}
+const NavBar = ({ navList }: NavBarProps) => {
+  return (
+    <nav
+      className="navbar navbar-expand-lg bg-body-tertiary"
+      data-bs-theme="dark"
+    >
+      <div className="container-fluid">
+        <Link to="/" className="navbar-brand">
+          CODERHO MOVIE
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            {navList.map((item, index) => (
+              <li className="nav-item" key={index}>
+                <Link
+                  to={item === "Home" ? "/" : `/${item}`}
+                  className={index === 0 ? "nav-link active" : "nav-link"}
+                  aria-current="page"
+                >
+                  {item}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
