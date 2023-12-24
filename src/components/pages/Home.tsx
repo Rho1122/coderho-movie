1d0da7c0b71ebadfd4f1569d445fe5f7d43c66c0
Rho1122 <kingrolsmack1@gmail.com>
2023-12-24 13:12:41 +0200

    Update

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -24,7 +24,7 @@ const Home = () => {
     setPageNo(pageNo === 1 ? 1 : pageNo - 1);
   };
 
-  const buttonMenu = ["Movie", "TV Series"];
+  const buttonMenu = ["Movie", "TV"];
 
   return (
     <>
